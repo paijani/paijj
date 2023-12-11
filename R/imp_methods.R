29d87c5170746a77bb80d1029cf34333dfd21f71
paijani <paijani.sheth@mail.utoronto.ca>
2023-12-11 03:43:53 -0500

    update again

--- R/imp_methods.R
+++ R/imp_methods.R
@@ -1,37 +1,28 @@
 #' @title Get available imputation methods based on data type
 #'
-#' @param data_type The type of data in the outcome column (eg. "binary", "numeric", "ordinal", "nominal")
-#' @param methods Built-in uni-variate imputation methods from the mice package
+#' @param data_type The type of data in the outcome column (supported data types: "binary", "numeric", "ordinal", "nominal")
 #'
-#' @return A character vector of available imputation methods
+#'
+#' @return A character vector of available imputation methods from the mice package, including their full name and mice code
 #'
 #'
 #' @examples methods <- imp_methods("binary")
-imp_methods <- function(data_type, level) {
+imp_methods <- function(data_type = "binary") {
   valid_data_types <- c("binary", "numeric", "ordinal", "nominal")
-  level <- c("1", "2")
 
-  if(!data_type %in% valid_data_types) {
-    stop("Invalid data type. Supported types: 'binary', 'numeric, 'ordinal', 'nominal")
-  }
-
-  if (!level %in% valid_levels) {
-    stop("Invalid level. Supported levels: '1', '2'")
+  if(!is.null(data_type) && !data_type %in% valid_data_types) {
+    stop("Invalid data type. Supported types: 'binary', 'numeric, 'ordinal', 'nominal'")
   }
 
   if(data_type == "numeric") {
-    return(c("Predictive mean matching (pmm)", "Weighted predictive mean matching (midastouch)", "Random sample (sample)", "Classification and regression trees (cart)", "Random forest (rf)", "Level-2 class predictive mean matching (2lonly.pmm)", "Level-1 normal heteroscedastic (2l.norm)", "Level-1 normal homoscedastic, linear mixed-effects model (2l.lmer)", "Level-2 class mean (2lonly.mean)", "Level-2 class normal (2lonly.norm)", "Unconditional mean imputation (mean)", "Bayesian linear regression (norm)", "Linear regression ignoring model error (norm.nob)", "Linear regression using bootstrap (norm.boot)", "Linear regression, predicted values (norm.predict)", "	Lasso linear regression (lasso.norm)", "Lasso select + linear regression (lasso.select.norm)", "	Imputation of quadratic terms (quadratic)", "	Random indicator for nonignorable data (ri)"))
+    return(c("Predictive mean matching (pmm)", "Weighted predictive mean matching (midastouch)", "Random sample (sample)", "Classification and regression trees (cart)", "Random forest (rf)", "Level-2 class predictive mean matching (2lonly.pmm)", "Level-1 normal heteroscedastic (2l.norm)", "Level-1 normal homoscedastic, linear mixed-effects model (2l.lmer)", "Level-1 normal homoscedastic, pan (2l.pan)", "Level-2 class mean (2lonly.mean)", "Level-2 class normal (2lonly.norm)", "Unconditional mean imputation (mean)", "Bayesian linear regression (norm)", "Linear regression ignoring model error (norm.nob)", "Linear regression using bootstrap (norm.boot)", "Linear regression, predicted values (norm.predict)", "	Lasso linear regression (lasso.norm)", "Lasso select + linear regression (lasso.select.norm)", "	Imputation of quadratic terms (quadratic)", "	Random indicator for nonignorable data (ri)"))
   } else if(data_type == "binary") {
-    return(c("Predictive mean matching (pmm)", "Weighted predictive mean matching (midastouch)", "Random sample (sample)", "Classification and regression trees (cart)", "Random forest (rf)", "Level-2 class predictive mean matching (2lonly.pmm)", "Logistic regression (logreg)", "Logistic regression with bootstrap (logreg.boot)", "Lasso logistic regression (lasso.logreg)","	Lasso select + logistic regression (lasso.select.logreg)", "Level-1 logistic, generalized linear mixed-effects model (2l.bin)"))
+    return(c("Predictive mean matching (pmm)", "Weighted predictive mean matching (midastouch)", "Random sample (sample)", "Classification and regression trees (cart)", "Random forest (rf)", "Level-2 class predictive mean matching (2lonly.pmm)", "Logistic regression (logreg)", "Logistic regression with bootstrap (logreg.boot)", "Lasso logistic regression (lasso.logreg)", "Lasso select + logistic regression (lasso.select.logreg)", "Level-1 logistic, generalized linear mixed-effects model (2l.bin)"))
   } else if(data_type == "ordinal") {
     return(c("Predictive mean matching (pmm)", "Weighted predictive mean matching (midastouch)", "Random sample (sample)", "Classification and regression trees (cart)", "Random forest (rf)", "Level-2 class predictive mean matching (2lonly.pmm)", "	Proportional odds model (polr)"))
   } else if(data_type == "nominal") {
     return(c("Predictive mean matching (pmm)", "Weighted predictive mean matching (midastouch)", "Random sample (sample)", "Classification and regression trees (cart)", "Random forest (rf)", "Level-2 class predictive mean matching (2lonly.pmm)", "Polytomous logistic regression (polyreg)", "Linear discriminant analysis (lda)"))
   }
 
-  if(level == "1") {
-    return(c("Level-1 normal heteroscedastic (2l.norm)", "Level-1 normal homoscedastic, linear mixed-effects model (2l.lmer)", "Level-1 normal homoscedastic, pan (2l.pan)", "Level-1 logistic, generalized linear mixed-effects model (2l.bin)"))
-  } else if (level == "2") {
-    return(c("Level-2 class mean (2lonly.mean)", "Level-2 class normal (2lonly.norm)", "Level-2 class predictive mean matching (2lonly.pmm)"))
-  }
+
 }
--- R/missing_gone.R
+++ R/missing_gone.R
@@ -3,12 +3,13 @@
 #' @param data A data frame that has the missing data
 #' @param threshold The threshold that represents the percentage of missing data in the variable.
 #' Variables with missing data percent frequency above this threshold will be removed.
-#'
+#' @param exclude A variable is deemed important in dataset and does not want removed, even if missingness is above threshold
 #' @return A data frame with variables removed based on the threshold
 #'
 #'
 #' @examples Using the "here" package to locate the example file:
 #' library(here)
+#' library(dplyr)
 #' data <- here("data-raw", "conflict_data.csv")
 #' Write in the function:
 #' data <- missing_gone(data, threshold = 0.1, exclude = "Maternal Mortality Rate")
@@ -31,7 +32,7 @@ missing_gone <- function(data, threshold = 0.1, exclude = NULL) {
 
     missing_ratio <- sum(is.na(data[[variables]])) / length(data[[variables]])
 
-    #if missingness is above threshold, don't change the variable
+    #if missingness is above threshold, remove from data set
     if (missing_ratio > threshold) {
       data <- data %>% select(-{{variables}})
     }
